a8859f722a62d32a90d0d783e1fb81fc8289a4f9
Minh Hieu <106766602+hius137@users.noreply.github.com>
2023-09-19 17:47:34 +0700

    UI

--- lib/utils/custom_thumbnail.dart
+++ lib/utils/custom_thumbnail.dart
@@ -9,62 +9,62 @@ class CustomThumbnail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-      child: Stack(
-        children: [
-          ClipRRect(
+    return Stack(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: ClipRRect(
             borderRadius: BorderRadius.circular(30),
             child: Image(
               image: AssetImage(imageMovie),
               fit: BoxFit.cover,
             ),
           ),
-          Positioned(
-            bottom: 15,
-            left: 26,
-            child: Text(
-              nameMovie,
-              style: GoogleFonts.beVietnamPro(
-                  fontSize: 18,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
+        ),
+        Positioned(
+          bottom: 15,
+          left: 26,
+          child: Text(
+            nameMovie,
+            style: GoogleFonts.beVietnamPro(
+                fontSize: 18,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
           ),
-          Positioned(
-            bottom: 20,
-            right: 26.49,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFF5C518),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: 3),
-                    child: Image(
-                      image: AssetImage(AppImage.ic_imdb),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 26.49,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFFF5C518),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 3),
+                  child: Image(
+                    image: AssetImage(AppImage.ic_imdb),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(3),
+                  child: Text(
+                    '8.5',
+                    style: GoogleFonts.beVietnamPro(
+                      fontSize: 6,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(3),
-                    child: Text(
-                      '8.5',
-                      style: GoogleFonts.beVietnamPro(
-                        fontSize: 6,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
